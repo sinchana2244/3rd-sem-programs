fbe4448e4c2ff21fa316661f46974ec814c597fc
sinchana2244 <153730418+sinchana2244@users.noreply.github.com>
2024-01-08 15:35:43 +0530

    ds 6th program

--- sinchu.c
+++ sinchu.c
@@ -1,75 +1,97 @@
 #include<stdio.h>
-#include<math.h>
-#define MAX 20
-struct stack
+#include<stdlib.h>
+#define SIZE 5
+int CQ[SIZE];
+int front=-1;
+int rear=-1,ch;
+int IsCQ_Full();
+int IsCQ_Empty();
+void CQ_Insert(int);
+void CQ_Delet();
+void CQ_Display();
+void main()
 {
-	int top;
-	float str[MAX];
-}s;
-char postfix[MAX];
-void push(float);
-float pop();
-int isoperand(char);
-float operate(float,float,char);
-int main()
-{
-	int i=0;
-	printf("Enter Expression:");
-	scanf("%s",postfix);
-	float ans,op1,op2;
-	while(postfix[i]!='\0')
+	printf("1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+	while(1)
 	{
-		if(isoperand(postfix[i]))
-			push(postfix[i]-48);
-		else
+		int ele;
+		printf("Enter your choice\n");
+		scanf("%d",&ch);
+		switch(ch)
 		{
-			op1=pop();
-			op2=pop();
-			ans=operate(op1,op2,postfix[i]);
-			push(ans);
-			printf("%f%c%f=%f\n",op2,postfix[i],op1,ans);
+			case 1:if(IsCQ_Full())
+			printf("Circular queue Overflow\n");
+			       else
+			       {
+				printf("Enter the element to be inserted\n");
+			 scanf("%d",&ele);CQ_Insert(ele);
+			       }
+			       break;
+			case 2:if(IsCQ_Empty())
+			printf("Circular Queue Underflow\n");
+			       else
+			       CQ_Delet();
+			       break;
+			case 3:if(IsCQ_Empty())
+			printf("Circular Queue Underflow\n");
+			       else
+			CQ_Display();
+			       break;
+			case 4:exit(0);
 		}
-		i++;
 	}
-	printf("%f",s.str[s.top]);
 }
-int isoperand(char x)
+void CQ_Insert(int item)
 {
-	if(x>='0'&& x<='9')
-		return 1;
-	else return 0;
+	if(front==-1)
+		front++;
+	rear=(rear+1)%SIZE;
+	CQ[rear]=item;
 }
-void push(float x)
+void CQ_Delet()
 {
-	if(s.top==MAX-1)
-		printf("Stack is full\nStack overflow\n");
+	int item;item=CQ[front];
+	printf("Deleted element is:%d",item);
+	front=(front+1)%SIZE;
+}
+void CQ_Display()
+{
+	int i;
+	if(front==-1)
+		printf("Circular Queue is Empty\n");
 	else
 	{
-		s.top++;
-		s.str[s.top]=x;
+		printf("Elements of circular queue are..\n");
+		for(i=front;i!=rear;i=(i+1)%SIZE);
+		{
+			printf("%d\t",CQ[i]);
+		}
+		printf("%d\n",CQ[i]);
 	}
 }
-float pop()
+int IsCQ_Full()
 {
-	if(s.top==-1)
-	{
-		printf("Stack is empty\nSTACK UNDERFLOW\n");
-	}
-		else
-	{
-		s.top--;
-		return s.str[s.top+1];
-	}
+	if(front==(rear+1)%SIZE)
+	return 1;
+	return 0;
 }
-float operate(float op1,float op2,char a)
+int IsCQ_Empty()
 {
-	switch(a)
+	if(front==-1)
+	return 1;
+	else if(front==rear)
 	{
-		case'+':return op2+op1;
-		case'-':return op2-op1;
-		case'*':return op2*op1;
-		case'/':return op2/op1;
-		case'n':return (op2,op1);
+		printf("Deleted element is:%d",CQ[front]);
+		front=-1;
+		return 1;
 	}
+	return 0;
 }
+
+
+			       
+
+					 
+
+		       
 
